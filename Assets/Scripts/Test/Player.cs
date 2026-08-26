a5f11da9531847abde9650fcdadf82ffa759b03e
Rel1fe <1749044002@qq.com>
2026-08-26 15:23:17 +0800

    优化了行为树可视化工具使用
    优化了行为树可视化工具使用

--- Assets/Scripts/Test/Player.cs
+++ Assets/Scripts/Test/Player.cs
@@ -1,29 +1,46 @@
 using RECode.REFramework;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private CharacterController controller;
+
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private float gravity = 9.8f;
+
+    private Vector2 input;
+
+    private void Awake()
+    {
+        controller= GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        input.x = Input.GetAxis("Horizontal");
+        input.y = Input.GetAxis("Vertical");
+        controller.Move(input * speed*Time.deltaTime);
+        controller.Move(Vector2.down*gravity*Time.deltaTime);
+    }
+
     private void OnEnable()
     {
-        EventCenter.Instance.AddListener("PlayerMove", Move);
         EventCenter.Instance.AddListener<(string, string, int)>("PlayerTest", Test);
     }
 
     private void OnDisable()
     {
-        EventCenter.Instance.RemoveListener("PlayerMove", Move);
         EventCenter.Instance.RemoveListener<(string, string, int)>("PlayerTest", Test);
     }
 
-    public void Move()
-    {
-        Debug.Log("Player Move");
-    }
 
     public void Test((string s1,string s2,int i) args)
     {
-        Debug.Log($"{args.s1},{args.s2},{args.i}");
+        UnityEngine.Debug.Log($"{args.s1},{args.s2},{args.i}");
     }
 }
